5aa2bcf6177520c885d4ef23b1d5b61d774c1a17
Nidhi Narendra <nidhi.narendra1@gmail.com>
2017-06-26 12:31:58 -0700

    added code to dynamically update data from the server (server has the data hardcoded)

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,10 @@
+// creating some dummy data in the server
+//instead of the hard code in the html file
+var data = [{item : 'get milk'}, {item: 'take kids to school'}, {item: 'send mail'}];
+
 module.exports = function(app){
   app.get('/todo', function(req, res){
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
   app.post('/todo', function(req, res){
